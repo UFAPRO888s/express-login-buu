0e2ba444808b3fab0b770a5bd6877f731431f297
VIP <88263706+UFAPRO888s@users.noreply.github.com>
2023-09-11 23:31:22 +0700

    s

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { database,firestore } from "../../firebase.js";
+import { database, firestore } from "../../firebase.js";
 // import "firebase/firestore";
-import { collection, getDocs } from "firebase/firestore"; 
+import { collection, getDocs ,query, where, orderBy,limit } from "firebase/firestore";
 import { push, ref, set, update, remove, onValue } from "firebase/database";
 
 import { verifyToken } from "./auth.js";
@@ -10,54 +10,81 @@ import { verifyToken } from "./auth.js";
 const productCollections = "products/";
 const productRef = ref(database, productCollections);
 
-const querySnapshot = await getDocs(collection(firestore, "data_center"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
+const data_centerRef = collection(firestore, "data_center");
 
-
-//get all products
-router.get("/", (req, res) => {
-  onValue(
-    productRef,
-    (snapshot) => {
-      if (snapshot.exists()) {
-        const productList = [];
-        snapshot.forEach((data) => {
-          productList.push({ id: data.key, ...data.val() });
-        });
-        console.log(productList);
-        res.json(productList);
-      } else {
-        res.status(404).send({ error: "product database is Empty" });
-      }
-    },
-    {
-      onlyOnce: true,
-    }
-  );
+router.get("/", async (req, res) => {
+  const mainDocs = [];
+  const querySnapshot = await getDocs(collection(firestore, "data_center"));
+  querySnapshot.forEach(async (doc) => {
+    mainDocs.push({ ...doc.data(), _id: doc.id });
+  });
+  //console.log(mainDocs)
+  res.json(mainDocs);
 });
 
 //get single product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
-  onValue(
-    ref(database, productCollections + id),
-    (snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-        res.json(snapshot.val());
-      } else {
-        res
-          .status(404)
-          .send({ error: "product does not exit with given id:" + id });
-      }
-    },
-    {
-      onlyOnce: true,
-    }
-  );
+  if(!id){return}
+  let queryDocs = []
+  const qZX = query(data_centerRef, where('categorie_id', '==', id), orderBy("Year"));
+  const querySnapshot = await getDocs(qZX);
+  querySnapshot.forEach(async (doc) => {
+    queryDocs.push({ ...doc.data(), _id: doc.id });
+  });
+  // const querySnapshot = await getDocs(collection(firestore, "data_center"));
+  // const snapshot = await querySnapshot.where('categorie_id', '==', id);
+  // if (snapshot.empty) {
+  //   console.log('No matching documents.');
+  //   return;
+ 
+  console.log(queryDocs);
+  res.json(queryDocs);
 });
+
+
+// //get all products
+// router.get("/", (req, res) => {
+//   onValue(
+//     productRef,
+//     (snapshot) => {
+//       if (snapshot.exists()) {
+//         const productList = [];
+//         snapshot.forEach((data) => {
+//           productList.push({ id: data.key, ...data.val() });
+//         });
+//         console.log(productList);
+//         res.json(productList);
+//       } else {
+//         res.status(404).send({ error: "product database is Empty" });
+//       }
+//     },
+//     {
+//       onlyOnce: true,
+//     }
+//   );
+// });
+
+// //get single product
+// router.get("/:id", (req, res) => {
+//   const id = req.params.id;
+//   onValue(
+//     ref(database, productCollections + id),
+//     (snapshot) => {
+//       if (snapshot.exists()) {
+//         console.log(snapshot.val());
+//         res.json(snapshot.val());
+//       } else {
+//         res
+//           .status(404)
+//           .send({ error: "product does not exit with given id:" + id });
+//       }
+//     },
+//     {
+//       onlyOnce: true,
+//     }
+//   );
+// });
 
 //add new product
 router.post("/", verifyToken, (req, res) => {
